fbcdcb6127192a58beddc544ec13461ec4a73e41
yam1er <mantynouyacoubou@gmail.com>
2022-06-02 18:25:18 -0700

    printf utility command

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+/*
+*Notice that final new line character is implicit
+*/
+printf("with proper grammar, but the outcome is a piece of art,\n");
+
+return (0);
+}
