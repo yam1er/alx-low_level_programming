47afd819e987c0ce9901adee11f7aba530212faa
yam1er <mantynouyacoubou@gmail.com>
2022-06-02 18:07:42 -0700

    Write program that write text out

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+/*
+*Notice that final new line character is implicit
+*/
+puts("\"Programming is like building a multilingual puzzle");
+
+return (0);
+}
