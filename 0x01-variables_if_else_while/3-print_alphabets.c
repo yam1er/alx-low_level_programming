b5734a9ab337ad4e745cdc9154d8059e591d65f3
yam1er <mantynouyacoubou@gmail.com>
2022-06-05 20:43:24 -0700

    fix

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/**
+	* print lowercase alphabet out
+	*/
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		putchar(alpha);
+	}
+	putchar('\n');
+	/**
+	* print uppercase alphabet out
+	*/
+	for (alpha = 'A'; alpha <= 'Z'; alpha++)
+	{
+		putchar(alpha);
+	}
+	putchar('\n');
+	return (0);
+}
