70948030b10f6484ed2d58b355ea9699ad9afa38
yam1er <mantynouyacoubou@gmail.com>
2022-06-05 21:36:11 -0700

    fix

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * return: alway (0) Success
+ * Return: always 0 (Success)
  */
 int main(void)
 {
@@ -11,7 +11,7 @@ int main(void)
 
 	for (alpha = 'a'; alpha <= 'z'; alpha++)
 	{
-		if ((alpha != 'e') || (alpha != 'q'))
+		if ((alpha != 'e') && (alpha != 'q'))
 		{
 			putchar(alpha);
 		}
