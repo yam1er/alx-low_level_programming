6d0fbe3931cbbe3605091df01530af6259621640
yam1er <mantynouyacoubou@gmail.com>
2022-06-05 21:51:45 -0700

    writting number from 0 to 10 command commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Successs
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+		printf("%d", number);
+	printf("\n");
+	return (0);
+}
