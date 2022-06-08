03e761be4592041f2bdcd964ca8f87fbec5430fd
yam1er <mantynouyacoubou@gmail.com>
2022-06-07 22:32:42 -0700

    print_alphabet_x10 function commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include <main.h>
+/**
+ * print_alphabet_x10 - fonction that return 10 times a lowercase alphabet
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
+	int compter;
+
+	for (compter = 0; compter < 10; compter++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+			_putchar(ch);
+		_putchar('\n');
+	}
+}
