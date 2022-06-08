8e83aa457db92d181c78027ea296e972d0fab5c3
yam1er <mantynouyacoubou@gmail.com>
2022-06-07 22:06:29 -0700

    la fonction alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet - It return the lowercase alphabet
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+		_putchar(\n);
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,5 @@
 #ifndef _MAIN_H
 #define _MAIN_H
 int _putchar(char);
+void print_alphabet(void);
 #endif
