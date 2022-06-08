2ff6f22b9dfea40f1e852e16b0a572786b0d984d
yam1er <mantynouyacoubou@gmail.com>
2022-06-08 09:17:51 -0700

    fix

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+/**
+ * _isalpha - checks for alphabetic character
+ * @ch: character to check
+ *
+ * Return: 0 or 1
+ */
+
+int _isalpha(int ch)
+{
+	if ((c >= 'a' && c <= '2') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
