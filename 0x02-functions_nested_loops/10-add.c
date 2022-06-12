4c1cc3c528dcec03d12986cd1ed06b26d3097e83
yam1er <mantynouyacoubou@gmail.com>
2022-06-12 07:17:27 -0700

    function add that return the sum of two int

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+/**
+ * add - add to interger and return the result
+ *
+ * @number1: first number of call
+ * @number2: second number of call
+ *
+ * Return: return the result
+ */
+
+int add(int number1, int number2)
+{
+	return (number1 + number2);
+}
