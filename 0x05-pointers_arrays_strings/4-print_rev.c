093bc0e14e644766bb8516bcb036bf98f2e81dea
yam1er <mantynouyacoubou@gmail.com>
2022-06-14 21:29:31 -0700

    print_rev function

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - print string in reverse
+ * @s: hold the first caracter adress
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int i, size, j;
+
+	size = 0;
+	for (i = 0; s[i] != 0; i++)
+	{
+		size++
+	}
+	for (j = size; j <= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
+}
