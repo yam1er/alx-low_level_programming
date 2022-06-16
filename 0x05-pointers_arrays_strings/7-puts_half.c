4cd64a861925aee8c128c67c56fb27259439b5c2
yam1er <mantynouyacoubou@gmail.com>
2022-06-15 22:17:50 -0700

    function that put the half of funtion

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * puts_half - prints the second half of a string
+ * @str: input string to print
+ *
+ * @*str: input pointer to string
+ *
+ */
+
+void puts_half(char *str)
+{
+	int size, value;
+
+	size = 0;
+	for (value = 0; str[value] != 0; value++)
+	{
+		size++;
+	}
+	if (size % 2 == 0)
+	{
+		for (value = size / 2; value < size; value++)
+		{
+			_putchar(str[value]);
+		}
+	}
+	else
+	{
+		for (value = (sie / 2) + 1; value < size; value++)
+			_putchar(str[value]);
+	}
+	_putchar('\n');
+}
