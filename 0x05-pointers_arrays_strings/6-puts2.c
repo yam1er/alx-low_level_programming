6c8a6f75e6d547e1469004e3e4e676ac1399bb75
yam1er <mantynouyacoubou@gmail.com>
2022-06-15 21:19:13 -0700

    function that put other caracter

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - put other caracter
+ * @str: hold de adress of the string to print
+ *
+ * Return: nothing
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+
+	for (i = 0; i != 0; i += 2)
+	{
+		_putchar(str[i]);
+	}
+}
