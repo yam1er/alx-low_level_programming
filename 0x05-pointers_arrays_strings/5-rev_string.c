e75bb77b08489cba906fbc23ed503958f4a17164
yam1er <mantynouyacoubou@gmail.com>
2022-06-15 20:18:03 -0700

    funtion that revere string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse string
+ * @s: hold the address of first caracter
+ *
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int i, j, size;
+
+	size = 0;
+	for (i = 0; s[i] != 0; i++)
+	{
+		size++;
+	}
+	for (j = 0; j < size; j++)
+	{
+		s[size - j - 1] = s[j];
+	}
+	if (size == 0)
+	{
+		s[0] = "";
+	}
+}
