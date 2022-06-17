55188edcd557b282eaa0a087487e11389f3170ce
yam1er <mantynouyacoubou@gmail.com>
2022-06-17 02:58:18 -0700

    print_array funtion

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_array - print n elements of an array of integer
+ * @a: hold the address of array
+ * @n: nomber of nteger to output
+ *
+ * Return: nothing
+ */
+
+void (int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(a[i] + 'i');
+		_putchar(',');
+		_putchar(' ');
+	}
+	_putchar('\n');
+}
