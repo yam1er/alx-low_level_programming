4768c851e0f200b31bf52937ed043b89686572b2
yam1er <mantynouyacoubou@gmail.com>
2022-06-17 06:13:58 -0700

    fi

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -15,10 +15,7 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		if (i < n - 2)
-			printf("%d, ", *(a + i));
-		else
-			printf("%d", *(a + i));
+		printf("%d, ", *(a + i));
 	}
 	printf("\n");
 }
