83e0fffa3bcbd6451ebe49a33ac3759b6af0db6d
yam1er <mantynouyacoubou@gmail.com>
2023-08-14 19:35:58 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+/**
+* main - Entry point
+*
+* Return: returns 0 when everything works fine
+*
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; ++n)
+	{
+		printf("%d", n);
+	}
+	putchar(10);
+	return (0);
+}
