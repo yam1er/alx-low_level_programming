29c5331e1cc0bf996d0f9e39d48248760d49d99e
yam1er <mantynouyacoubou@gmail.com>
2023-08-18 18:55:08 -0700

    Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - function that multiplies tzo integers
+ * @a: first interger for operation
+ * @b: second interger for operation
+ *
+ * Return: result of operation
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
