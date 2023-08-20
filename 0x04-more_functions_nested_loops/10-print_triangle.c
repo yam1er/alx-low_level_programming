2bb4f488cef5b8a7dd6a2dffc21a7e9f56598fd8
yam1er <mantynouyacoubou@gmail.com>
2023-08-19 22:51:57 -0700

    Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_triangle - print a triangle
+ * @size: size of the triangle
+ *
+ * Return: nothing
+ */
+
+void print_triangle(int size)
+{
+	int tri_size, height, c;
+
+	if (size > 0)
+	{
+	for (height = 0; height < size; height++)
+	{
+		for (tri_size = 0; tri_size <= (size - height - 1); tri_size++)
+		{
+			_putchar(' ');
+		}
+		for (c = 0; c < height + 1; c++)
+		{
+			_putchar('#');
+		}
+		_putchar(10);
+	}
+	}
+	else
+		_putchar(10);
+}
