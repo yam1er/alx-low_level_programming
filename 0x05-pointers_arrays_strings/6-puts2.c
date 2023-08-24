7b41479604ee613d010c288ca7910b95249194a9
yam1er <mantynouyacoubou@gmail.com>
2023-08-24 08:05:13 -0700

    fix

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,10 +9,14 @@
 
 void puts2(char *str)
 {
-	while (*str != '\0')
+	int len;
+
+	len = 0;
+	while (*(str + len) != '\0')
 	{
-		_putchar(*str);
-		str += 2;
+		if (len % 2 == 0)
+		_putchar(*(str + len));
+		len++;
 	}
 	_putchar(10);
 }
