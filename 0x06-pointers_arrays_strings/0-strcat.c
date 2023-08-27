11ad95f200c5d2c25c62f3c6c0a7b689288dec91
yam1er <mantynouyacoubou@gmail.com>
2023-08-26 18:19:14 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strcat - appends src string to dest
+ * @dest: fisrt string
+ * @src: last string to append to dest
+ *
+ * Return: a pointer to the resulting string dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i;
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+
+}
