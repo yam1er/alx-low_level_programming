fe3662ba137dae723914cf68104d6b9dd747f9f7
yam1er <mantynouyacoubou@gmail.com>
2023-09-21 16:06:07 -0700

    0. Print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include "list.h"
+
+/**
+ * print_list - print all the elements of a list_t list
+ * @h: node to print
+ *
+ * Return: a formated caracter
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t count_node = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		} else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		count_node++;
+		h = h->next;
+	}
+
+	return (count_node);
+
+}
