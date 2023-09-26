9003f1da34b9c744591ee9ef441804a9894d983d
yam1er <mantynouyacoubou@gmail.com>
2023-09-25 17:25:41 -0700

    fixed

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -28,7 +28,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	} else
 	{
 		current = *head;
-		while (current != NULL)
+		while (current->next != NULL)
 		{
 			current = current->next;
 		}
