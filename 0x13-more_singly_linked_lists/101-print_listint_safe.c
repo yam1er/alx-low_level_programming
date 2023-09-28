4b5e5660bc0ac75a008f57183e4dfbd829d7ab7d
yam1er <mantynouyacoubou@gmail.com>
2023-09-27 17:25:47 -0700

    Print (safe version)

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -10,20 +10,54 @@
 
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *current = head;
+	const listint_t *current;
 	size_t count = 0;
+	const listint_t *check;
+
+	current = head;
 
 	while (current != NULL)
 	{
-		printf("[%p] %d\n", (void *)current, current->n);
 		count++;
-		current = current->next;
+		printf("[%p] %\n", (void *)current, current->n);
 
-		if (count > 1024)
+		check = head;
+		for (size_t i = 0; i < count; i++)
 		{
-			printf("-> [%p] %d\n", (void *)current, current->n);
-			exit(98);
+			if (check == current)
+			{
+				printf("-> [%p] %d\n", (void *)current, current->n);
+				return (count);
+			}
+			check = check->next;
 		}
+
+		current = current->next;
 	}
 	return (count);
+}
+
+
+/**
+ * free_listint_safe - Frees a listint_t linked list safely
+ * @h: A pointer to a pointer to the head of the list
+ */
+
+void free_listint_safe(listint_t **h)
+{
+	listint_t *current;
+	listint_t *temp;
+
+	if (h == NULL || *h == NULL)
+		return;
+
+	current = *h;
+
+	while (current != NULL)
+	{
+		temp = current;
+		current = current->next;
+		free(temp);
+		*h = NULL;
+	}
 }
