479d1854f1ea59963c2ee1e6f674f651236dea2b
yam1er <mantynouyacoubou@gmail.com>
2023-11-09 18:39:07 -0800

    delete node function

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - Deletes a node from a list
+ * @head: pointer to the head
+ * @index: The index of the node to delete
+ *
+ * Return: -1 or 1
+ */
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *temp = *head;
+
+	if (*head == NULL)
+		return (-1);
+
+	for (; index != 0; index--)
+	{
+		if (temp == NULL)
+			return (-1);
+		temp = temp->next;
+	}
+
+	if (temp == *head)
+	{
+		*head = temp->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+	}
+
+	else
+	{
+		temp->prev->next = temp->next;
+		if (temp->next != NULL)
+			temp->next->prev = temp->prev;
+	}
+
+	free(temp);
+	return (1);
+}
