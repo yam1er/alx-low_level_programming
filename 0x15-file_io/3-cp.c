c6d3543b53007efd8a58d5934a6ea379787968c2
yam1er <mantynouyacoubou@gmail.com>
2023-11-09 16:02:19 -0800

    dooubly linked list project

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - copies the content of one file to another
+ * @argc: argument count
+ * @argv: arguments passed
+ *
+ * Return: 1 on success, exit otherwise
+ */
+
+int main(int argc, char *argv[])
+{
+
+}
